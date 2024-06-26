c98a79caab250a8562cc4fc455d711aac721bb32
vinicius <vinicius.quimica@gmail.com>
2024-06-26 20:44:12 -0300

    show notes

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
-    }
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -41,10 +41,10 @@ class UsersController {
      }
 
      user.name = name ?? user.name
-     user.email = email ?? user.email
+     user.email= email ?? user.email
 
      if(password && !old_password) {
-      throw new AppError("Você precisa informar a senha antiga para definir a nova senha!")
+      throw new AppError("Você informar a senha antiga para definir a nova senha")
      }
 
      if(password && old_password) {
